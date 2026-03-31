9a0351af0ca9683532e29124d53fff799d5329b4
Ayush Jha <koffeenkanvas@gmail.com>
2026-03-31 20:52:12 +0530

    Fix: Default scaling to 80% and added audio diagnostics

--- fix-midi-paths.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let js = fs.readFileSync('main.js', 'utf8');
-
-// Update to clean filenames for Vercel
-js = js.replace(/\/Hey-Jude-1\.mid/g, '/hey-jude.mid');
-js = js.replace(/\/Billie Eilish - Lovely\.mid/g, '/lovely-full.mid');
-
-fs.writeFileSync('main.js', js);
-console.log('main.js MIDI paths updated.');
